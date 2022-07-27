f8120f45c200f67ecf3f41a2d29a0ed9fb015f42
Ankita Luthra <ankita.luthra@walmart.com>
2022-07-27 22:52:30 +0530

    uses property name from ProducerConfig class

--- src/main/java/com/personal/sessions/Normal/Producer.java
+++ src/main/java/com/personal/sessions/Normal/Producer.java
@@ -1,6 +1,7 @@
 package com.personal.sessions.Normal;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
@@ -11,9 +12,9 @@ public class Producer {
 
     public Producer() {
         Properties properties = new Properties();
-        properties.put("bootstrap.servers", "localhost:9092");
-        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
         kafkaProducer = new KafkaProducer(properties);
     }
